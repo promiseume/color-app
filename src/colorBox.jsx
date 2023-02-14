4a226dc7d39fa06f05f1b418077eb7d5d528ea39
Ngozi Promise Ume <ngoziume1@gmail.com>
2023-02-14 16:39:38 +0100

    added copy to billboard

--- src/colorBox.jsx
+++ src/colorBox.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import './colorBox.css'
+import './colorBox.css';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 export default function ColorBox({backgroundcolor,name}) {
   return (
+    <CopyToClipboard text={backgroundcolor}>
     <div style={{background: backgroundcolor}} className='ColorBox'>
-      <span>{name}</span>
-      <span>More</span>
+   <div className='copy-container'>
+   <div className='box-container'> <span>{name}</span></div>
+     <button className='copy-button'>Copy</button>
+</div>
+      <span className='see-more'>More</span>
     </div>
+    </CopyToClipboard>
   )
 }
