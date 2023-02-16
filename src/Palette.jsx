a60d498bc7efa221fd03a2da64dfcf17a63d4ee8
Ngozi Promise Ume <ngoziume1@gmail.com>
2023-02-16 18:39:55 +0100

    added style to slider

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -1,18 +1,20 @@
-import React, {useEffect,useState} from 'react';
+import React, {useState} from 'react';
 import ColorBox from './colorBox.jsx';
+import 'rc-slider/assets/index.css';
 import './palette.css';
 import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 
 export default function Palette({palette}) {
   const [level, setLevel] = useState(500);
   const handleLevelChange =(level)=>{
-    setLevel(level);
+    setLevel(()=>level);
   }
   return (
     <div className='Palette'>
-       <div className='Palette-color'>
+      <div className='slider'>
         <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={handleLevelChange} step={100}/>
+        </div>
+       <div className='Palette-color'>
         {palette.colors[level].map(color => <ColorBox backgroundcolor={color.hex} name={color.name} />)}
        </div>
     </div>
