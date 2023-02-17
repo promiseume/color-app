1b8a36127c22401ff3dddad9e0143b4e4e1e6444
Ngozi Promise Ume <ngoziume1@gmail.com>
2023-02-17 17:11:16 +0100

    added snackbar for notification

--- src/navbar.js
+++ src/navbar.js
@@ -5,6 +5,7 @@ import Slider from 'rc-slider';
 import { Select } from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 
 export default function Navbar({level, handleLevelChange,handleColorFormat}) {
@@ -12,8 +13,13 @@ export default function Navbar({level, handleLevelChange,handleColorFormat}) {
     const [open, setOpen] = useState(false);
     const handleFormatChange =(e)=>{
         setFormat(e.target.value);
+        setOpen(true);
         handleColorFormat(e.target.value);
       }
+        const closeSnackbar = () => {
+            setOpen(false);
+        }
+
   return (
     <nav className='navbar'>
         <div className='logo'>
@@ -32,6 +38,30 @@ export default function Navbar({level, handleLevelChange,handleColorFormat}) {
                 <MenuItem value='rgba'>RGBA : (255,255,255, 0.1)</MenuItem>
             </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={open}
+          autoHideDuration={3000}
+          message={
+            <span id='message-id'>
+              Format Changed To {format.toUpperCase()}
+            </span>
+          }
+          ContentProps={{
+            "aria-describedby": "message-id"
+          }}
+          onClose={closeSnackbar}
+          action={[
+            <IconButton
+              onClick={closeSnackbar}
+              color='inherit'
+              key='close'
+              aria-label='close'
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
     </nav>
   )
 }
