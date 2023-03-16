57aa22730e4b27ebfd520ef9b19fe95765635e2a
Ngozi Promise Ume <ngoziume1@gmail.com>
2023-03-16 18:00:05 +0100

    refactor color box

--- src/colorBox.jsx
+++ src/colorBox.jsx
@@ -67,9 +67,59 @@ const styles = {
     border: 'none',
     marginBottom: '-3.5px',
     opacity: '0'
+  },
+  copyOverlay :{
+    opacity: "0",
+    zIndex:"0",
+    width: "100%",
+    height: "100%",
+    transition: "transform 0.6s ease-in-out",
+    transform: "scale(0.1)"
+  },
+  showOverlay :{
+    opacity: "1",
+  transform: "scale(50)",
+  zIndex: "10",
+  position: "absolute",
+  },
+  copyMsg : {
+    position: "fixed",
+    left: "0",
+    right: "0",
+    top: "0",
+    bottom: "0",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    fontSize: "4rem",                
+    transform: "scale(0.1)",
+    opacity: "0",
+    color: "white",
+    "& h1":{
+      fontWeight: "400",
+      textShadow: "1px 2px black",
+      background: "rgba(255, 255, 255, 0.2)",
+      width: "100%",
+      textAlign: "center",
+      marginBottom: "0",
+      padding: "1rem",
+      textTransform: "uppercase"
+
+    },
+    "& p":{
+      fontSize: "2rem",
+      fontWeight: "100"
+    }
+  },
+  showCopyMsg :{
+    opacity: "1",
+    transform: "scale(1)",
+    zIndex: "25",
+    transition: "all 0.4s ease-in-out",
+    transitionDelay: "0.3s"
   }
 };
-
 
 function ColorBox(props) {
   const { classes } = props;
@@ -84,9 +134,9 @@ function ColorBox(props) {
       <div style={{ background: props.backgroundcolor,height:props.height }} className={classes.ColorBox}>
         <div
           style={{ background: props.backgroundcolor }}
-          className={`copy-overlay ${copied && "show"}`}
+          className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}
         />
-        <div className={`copy-msg ${copied && "show"}`}>
+        <div className={`${classes.copyMsg} ${copied && classes.showCopyMsg}`}>
           <h1>Copied!</h1>
           <p className={classes.copyText}>{props.backgroundcolor}</p>
         </div>
