be47fc17a214f2929756e2c894e51c608e8d32ea
Ngozi Promise Ume <ngoziume1@gmail.com>
2023-05-03 19:15:50 +0100

    added force flag to build command

--- src/styles/paletteListStyles.js
+++ src/styles/paletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 export default {
     root: {
       backgroundColor: "blue",
@@ -14,6 +15,18 @@ export default {
       alignItems: "flex-start",
       flexDirection: "column",
       flexWrap: "wrap",
+      [sizes.down("xl")]: {
+        width: "70%",
+      },
+      [sizes.down("lg")]: {
+        width: "60%",
+      },
+      [sizes.down("md")]: {
+        width: "50%",
+      },
+      [sizes.down("sm")]: {
+        width: "75%",
+      }
     },
     nav: {
     display: "flex",
@@ -31,6 +44,12 @@ export default {
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
     gridGap: "5%",
-    margin:"10px"
+    margin:"10px",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+    },
     }
   }
